6be8e6b5aae9023fe3000191dc0152a7101d1620
Chad <chad1020@gmail.com>
2025-10-28 00:02:45 -0500

    feat: added dashboard, get user and prisma function

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,16 +1,56 @@
-import { BarChart3 } from "lucide-react";
-
+import { UserButton } from "@stackframe/stack";
+import { BarChart3, Package, Plus, Settings } from "lucide-react";
+import Link from "next/link";
 export default function Sidebar({
   currentPath = "/dashboard",
 }: {
   currentPath: string;
 }) {
-  return <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen">
-    <div className="mb-8">
-      <div className="flex item-center space-x-2 mb-4">
-        <BarChart3 className="w-10 h-10S"/>
+  const navigation = [
+    { name: "Dashboard", href: "/dashboard", icon: BarChart3 },
+    { name: "Inventory", href: "/inventory", icon: Package },
+    { name: "Add Product", href: "/add-product", icon: Plus },
+    { name: "Settings", href: "/settings", icon: Settings },
+  ];
+
+  return (
+    <div className="fixed left-0 top-0 bg-gray-900 text-white w-64 min-h-screen">
+      <div className="mb-8">
+        <div className="flex item-center space-x-2 mb-4">
+          <BarChart3 className="w-7 h-7" />
+
+          <span className="text-lg font-semibold">Inventory App</span>
+        </div>
       </div>
-      <span className="text-lg font-semibold">Inventory App</span>
-    </div>
-  </div>;
+      <nav className="space-y-1">
+        <div className="text-xs font-semibold text-gray-400 uppercase">
+          Inventory
+        </div>
+        {navigation.map((item) => {
+          const IconComponent = item.icon;
+          const isActive = currentPath === item.href;
+          return (
+            <Link
+              href={item.href}
+              key={item.href}
+              className={`flex items-center space-x-3 py-2 px-3 rounded-lg ${
+                isActive
+                  ? "bg-purple-100 text-gray-800"
+                  : "hover:bg-gray-800 text-gray-300"
+              }`}
+            >
+              <IconComponent className="w-5 h-5" />
+              <span className="text-sm">{item.name}</span>
+            </Link>
+          );
+        })}
+      </nav>
+
+<div className="absolute bottom-0 left-0 right-0 p-6 borter-t border-gray-700">
+  <div className="flex items-center justify-between">
+    <UserButton showUserInfo />
+  </div>
+</div>
+</div>
+);
 }
