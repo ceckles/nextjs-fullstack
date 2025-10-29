0ae2dd5f7950d98508d442f73c39f657fc9be00e
Chad <chad1020@gmail.com>
2025-10-28 19:08:59 -0500

    feat: added stock lvls and colors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -39,11 +39,11 @@ export default async function DashboardPage() {
     0,
   );
 
-  console.log("total Products: ", totalProducts);
-  console.log("Low Stock: ", lowStock);
-  console.log("all Products: ", allProducts);
-  console.log("Recent Products: ", recent);
-  console.log("Total Value: ", totalValue);
+  // console.log("total Products: ", totalProducts);
+  // console.log("Low Stock: ", lowStock);
+  // console.log("all Products: ", allProducts);
+  // console.log("Recent Products: ", recent);
+  // console.log("Total Value: ", totalValue);
   return (
     <div className="min-h-screen bg-gray-50">
       <Sidebar currentPath="/dashboard" />
@@ -104,6 +104,38 @@ export default async function DashboardPage() {
                   <TrendingUp className="w-3 h-3 text-green-600 ml-1" />
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+          {/*Stock Levels*/}
+          <div className="bg-white rounded-lg border border-gray-200 p-6">
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-lg font-semibold text-gray-900">
+                Stock Levels
+              </h2>
+            </div>
+            <div className="space-y-3">
+              {recent.map((product) => {
+                const stockLevel = product.quantity === 0 
+                ? 0
+                : product.quantity <= (product.lowStockThreshold || 5)
+                ? 1
+                : 2;
+
+                const bgColors = ["bg-red-600","bg-yellow-600","bg-green-600"]
+                const textColors = ["text-red-600","text-yellow-600","text-green-600"]
+
+                return (
+                  <div key={product.id} className="flex items-center justify-between p-3 rounded-lg bg-gray-50">
+                    <div className="flex items-center space-x-3">
+                      <div className={`w-3 h-3 rounded-full ${bgColors[stockLevel]}`}/>
+                      <span className="text-sm font-medium text-gray-900">{product.name}</span>
+                    </div>
+                    <div className={`text-sm font-medium ${textColors[stockLevel]}`}>{product.quantity} units</div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
